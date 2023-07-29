2ea2f6e2f859ce59425af3794ec133fbedc84391
Koshi41 <kossy1009@icloud.com>
2023-07-29 15:43:23 +0900

    ポートフォリオサイトの開発を進めました

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
-const da = document.getElementById("nn");
 const btn = document.getElementById("button1");
 const pnda = document.getElementById("pimg");
 
 pnda.width = 1;
 pnda.height = 1;
 
+let count = 0;
+
 btn.onclick = function () {
   pnda.width += 30;
   pnda.height += 30;
   console.dir(pnda);
+  count += 1;
 };
+
+let qw1 = document.getElementById("qwe");
+
+if (count >= 5) {
+  qw1.textContent = "あなたのおすすめの曲を教えてください！";
+  console.log(qw1);
+}
